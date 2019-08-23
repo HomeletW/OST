17b299d288dd0ccb7cb17524e6f6a02e5b7bc412
HomeletWei <homeletwei@gmail.com>
2019-08-23 19:13:39 -0400

    commit

--- Drawer.py
+++ Drawer.py
@@ -1,7 +1,9 @@
 import Main
+from PIL import Image, ImageDraw
+from typing import *
 
 
-Coordinates = {
+coordinates = {
 	"Size": (0, 0),
 	"Offset": (0, 0),
 	"OST_DateOfIssue": (0, 0, 0, 0, 10),
@@ -22,6 +24,7 @@ Coordinates = {
 	"DateOfEntry_Y": (0, 0, 0, 0, 10),
 	"DateOfEntry_M": (0, 0, 0, 0, 10),
 	"DateOfEntry_D": (0, 0, 0, 0, 10),
+	# (x, y, width, height)
 	"Course": (0, 0, 0, 0),
 	# (x_offset, width)
 	"Course_date_offset": (0, 0),
@@ -45,7 +48,177 @@ Coordinates = {
 	"Authorization": (0, 0, 0, 0, 10),
 }
 
+# alignment flag
+flag_left = 0
+flag_center = 1
+flag_right = 2
 
-def draw(info: Main.OST_info):
-	# get the offset of the paper and create a new Image
-	pass
+
+def draw(info: Main.OST_info, font):
+	img = Image.new(mode="LA", size=coordinates["Size"])
+	drawer = ImageDraw.Draw(img)
+	# draw the courses
+	# draw OST Date of Issue
+	draw_text(drawer=drawer,
+	          text=info.OST_date_of_issue(),
+	          config=coordinates["OST_DateOfIssue"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw Surname
+	draw_text(drawer=drawer,
+	          text=info.surname(),
+	          config=coordinates["Surname"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw Given Name
+	draw_text(drawer=drawer,
+	          text=info.given_name(),
+	          config=coordinates["GivenName"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw OEN
+	draw_text(drawer=drawer,
+	          text=info.OEN(),
+	          config=coordinates["OEN"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw student number
+	draw_text(drawer=drawer,
+	          text=info.student_number(),
+	          config=coordinates["StudentNumber"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw gender
+	draw_text(drawer=drawer,
+	          text=info.gender(),
+	          config=coordinates["Gender"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw date of birth year
+	draw_text(drawer=drawer,
+	          text=info.date_of_birth_y(),
+	          config=coordinates["DateOfBirth_Y"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw date of birth month
+	draw_text(drawer=drawer,
+	          text=info.date_of_birth_m(),
+	          config=coordinates["DateOfBirth_M"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw date of birth day
+	draw_text(drawer=drawer,
+	          text=info.date_of_birth_d(),
+	          config=coordinates["DateOfBirth_D"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw name of district school board
+	draw_text(drawer=drawer,
+	          text=info.name_of_district_school_board(),
+	          config=coordinates["NameOfDSB"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw number of district school board
+	draw_text(drawer=drawer,
+	          text=info.district_school_board_number(),
+	          config=coordinates["NumberOfDSB"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw name of school
+	draw_text(drawer=drawer,
+	          text=info.name_of_school(),
+	          config=coordinates["NameOfSchool"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw number of school
+	draw_text(drawer=drawer,
+	          text=info.school_number(),
+	          config=coordinates["NumberOfSchool"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw date of entry year
+	draw_text(drawer=drawer,
+	          text=info.date_of_entry_y(),
+	          config=coordinates["DateOfEntry_Y"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw date of entry month
+	draw_text(drawer=drawer,
+	          text=info.date_of_entry_m(),
+	          config=coordinates["DateOfEntry_M"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw date of entry day
+	draw_text(drawer=drawer,
+	          text=info.date_of_entry_d(),
+	          config=coordinates["DateOfEntry_D"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw specialized program
+	draw_text(drawer=drawer,
+	          text=info.specialized_program(),
+	          config=coordinates["SpecializedProgram"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw diploma or certificate
+	draw_text(drawer=drawer,
+	          text=info.diploma_or_certificate(),
+	          config=coordinates["DiplomaOrCertificate"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# draw diploma or certificate date year
+	draw_text(drawer=drawer,
+	          text=info.diploma_or_certificate_date_of_issue_y(),
+	          config=coordinates["DiplomaOrCertificate_DateOfIssue_Y"],
+	          font=font,
+	          alignment_flag=flag_center)
+	# draw date of birth month
+	draw_text(drawer=drawer,
+	          text=info.diploma_or_certificate_date_of_issue_m(),
+	          config=coordinates["DiplomaOrCertificate_DateOfIssue_M"],
+	          font=font,
+	          alignment_flag=flag_center)
+	# draw authorization
+	draw_text(drawer=drawer,
+	          text=info.authorization(),
+	          config=coordinates["Authorization"],
+	          font=font,
+	          alignment_flag=flag_left)
+	# save the image
+	img.save(info.full_name() + " OST.png")
+
+
+def draw_text(drawer, text, config: List[int, int, int, int, int], font, offset=coordinates["Offset"], alignment_flag=flag_left):
+	# determine how long the text is going to be and apply the alignment flag
+	width, height = drawer.textsize(text=text, font=font, spacing=0)
+	# get the final position of the text and apply the alignment
+	offset_x, offset_y = offset
+	box_x, box_y, box_width, box_height, text_size = config
+	x = offset_x + box_x
+	if alignment_flag is flag_right:
+		x = x + box_width - width
+	elif alignment_flag is flag_center:
+		x = x + box_width / 2 - width / 2
+	else:
+		x = x
+	y = offset_y + box_y + box_height / 2 - height / 2
+	drawer.text(xy=(x, y), text=text)
+
+
+def plan_courses(drawer, courses: List[Main.Course], font, font_size, spacing):
+	# plan the font
+	course_x, course_y, course_width, course_height = coordinates["Course"]
+	offset_x, offset_y = coordinates["Offset"]
+	course_x += offset_x
+	course_y += offset_y
+	# get the height of the font
+	_, test_height = drawer.textsize(text="ABCD", font=font, spacing=0)
+	# minus the spacing from the top
+	avaliHeight = course_height - spacing
+	# calculate how much space does the course takes
+	unit_height = test_height + spacing
+	total_height = unit_height * len(courses)
+	# calculate how many page does it takes
+	pages, reminder = int(total_height // avaliHeight), total_height % avaliHeight
+	if reminder > spacing:
+		pages += 1
--- Main.py
+++ Main.py
@@ -2,13 +2,13 @@ from typing import *
 
 
 default_value = {
-	"date": (2000, 1, 1),
-	"date_simplify": (2000, 1),
+	"date": ("2000", "1", "1"),
+	"date_simplify": ("2000", "1"),
 	"name_of_district_school_board": "Toronto Private Inspected",
 	"district_school_board_number": "",
 	"name_of_school": "McCanny Secondary School",
 	"school_number": "668901",
-	"authorization": "Dr. Alireza Rafiee"
+	"authorization": "Dr. Alireza Rafiee",
 }
 
 # "course_code": ["course_title", "course_level"]
@@ -49,39 +49,150 @@ class OST_info:
 	
 	
 	def __init__(self,
-	             OST_date_of_issue: List[int, int, int] = default_value["date"],
+	             OST_date_of_issue: List[str, str, str] = default_value["date"],
 	             name: List[str, str] = ("", ""),
 	             OEN: str = "",
 	             student_number: str = "",
 	             gender: str = "M",
-	             date_of_birth: List[int, int, int] = default_value["date"],
+	             date_of_birth: List[str, str, str] = default_value["date"],
 	             name_of_district_school_board: str = default_value["name_of_district_school_board"],
 	             district_school_board_number: str = default_value["district_school_board_number"],
 	             name_of_school: str = default_value["name_of_school"],
 	             school_number: str = default_value["school_number"],
-	             date_of_entry: List[int, int, int] = default_value["date"],
+	             date_of_entry: List[str, str, str] = default_value["date"],
 	             community_involvement_flag=True,
 	             provincial_secondary_school_literacy_requirement_flag=True,
 	             specialized_program: str = "",
 	             diploma_or_certificate: str = "",
-	             diploma_or_certificate_date_of_issue: List[int, int] = default_value["date_simplify"],
+	             diploma_or_certificate_date_of_issue: List[str, str] = default_value["date_simplify"],
 	             authorization: str = default_value["authorization"]
 	             ):
-		self.OST_date_of_issue = [OST_date_of_issue[0], OST_date_of_issue[1], OST_date_of_issue[2]]
-		self.name = [name[0], name[1]]
-		self.OEN = OEN
-		self.student_number = student_number
-		self.gender = gender
-		self.date_of_birth = [date_of_birth[0], date_of_birth[1], date_of_birth[2]]
-		self.name_of_district_school_board = name_of_district_school_board
-		self.district_school_board_number = district_school_board_number
-		self.name_of_school = name_of_school
-		self.school_number = school_number
-		self.date_of_entry = [date_of_entry[0], date_of_entry[1], date_of_entry[2]]
-		self.course_list = []
-		self.community_involvement_flag = community_involvement_flag
-		self.provincial_secondary_school_literacy_requirement_flag = provincial_secondary_school_literacy_requirement_flag
-		self.specialized_program = specialized_program
-		self.diploma_or_certificate = diploma_or_certificate
-		self.diploma_or_certificate_date_of_issue = [diploma_or_certificate_date_of_issue[0], diploma_or_certificate_date_of_issue[1]]
-		self.authorization = authorization
+		self._OST_date_of_issue = [OST_date_of_issue[0], OST_date_of_issue[1], OST_date_of_issue[2]]
+		self._name = [name[0], name[1]]
+		self._OEN = OEN
+		self._student_number = student_number
+		self._gender = gender
+		self._date_of_birth = [date_of_birth[0], date_of_birth[1], date_of_birth[2]]
+		self._name_of_district_school_board = name_of_district_school_board
+		self._district_school_board_number = district_school_board_number
+		self._name_of_school = name_of_school
+		self._school_number = school_number
+		self._date_of_entry = [date_of_entry[0], date_of_entry[1], date_of_entry[2]]
+		self._community_involvement_flag = community_involvement_flag
+		self._provincial_secondary_school_literacy_requirement_flag = provincial_secondary_school_literacy_requirement_flag
+		self._specialized_program = specialized_program
+		self._diploma_or_certificate = diploma_or_certificate
+		self._diploma_or_certificate_date_of_issue = [diploma_or_certificate_date_of_issue[0], diploma_or_certificate_date_of_issue[1]]
+		self._authorization = authorization
+		self._course_list = []
+		self._course_font_size = 10
+		self._course_spacing = 5
+	
+	
+	def course(self):
+		return self._course_list
+	
+	
+	def course_font_size(self):
+		return self._course_font_size
+	
+	
+	def course_spacing(self):
+		return self._course_spacing
+	
+	
+	def full_name(self):
+		return self._name[0] + " " + self._name[1]
+	
+	
+	def OST_date_of_issue(self):
+		year, month, day = self._OST_date_of_issue
+		return month + " " + day + " " + year
+	
+	
+	def surname(self):
+		return self._name[0]
+	
+	
+	def given_name(self):
+		return self._name[1]
+	
+	
+	def OEN(self):
+		return self._OEN
+	
+	
+	def student_number(self):
+		return self._student_number
+	
+	
+	def gender(self):
+		return self._gender
+	
+	
+	def date_of_birth_y(self):
+		return self._date_of_birth[0]
+	
+	
+	def date_of_birth_m(self):
+		return self._date_of_birth[1]
+	
+	
+	def date_of_birth_d(self):
+		return self._date_of_birth[2]
+	
+	
+	def name_of_district_school_board(self):
+		return self._name_of_district_school_board
+	
+	
+	def district_school_board_number(self):
+		return self._district_school_board_number
+	
+	
+	def name_of_school(self):
+		return self._name_of_school
+	
+	
+	def school_number(self):
+		return self._school_number
+	
+	
+	def date_of_entry_y(self):
+		return self._date_of_entry[0]
+	
+	
+	def date_of_entry_m(self):
+		return self._date_of_entry[1]
+	
+	
+	def date_of_entry_d(self):
+		return self._date_of_entry[2]
+	
+	
+	def community_involvement(self):
+		return self._community_involvement_flag
+	
+	
+	def provincial_secondary_school_literacy_requirement(self):
+		return self._provincial_secondary_school_literacy_requirement_flag
+	
+	
+	def specialized_program(self):
+		return self._specialized_program
+	
+	
+	def diploma_or_certificate(self):
+		return self._diploma_or_certificate
+	
+	
+	def diploma_or_certificate_date_of_issue_y(self):
+		return self._diploma_or_certificate_date_of_issue[0]
+	
+	
+	def diploma_or_certificate_date_of_issue_m(self):
+		return self._diploma_or_certificate_date_of_issue[1]
+	
+	
+	def authorization(self):
+		return self._authorization
